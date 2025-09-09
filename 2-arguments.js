ed8f68bb7955a6d24b6dc260e94787db1b7297c4
Saadallah-design <saadalla.publishing@gmail.com>
2025-09-09 10:20:55 +0700

    Adding the 2-arguments.js file using console.log in the outer function.

---
+++ 2-arguments.js
@@ -0,0 +1,26 @@
+// In these lines of code I will try to implement a count arguments counter
+// first I will start by declaring a function that takes no arguments
+// then inside it I will declare a variable that equals arguments length
+
+function argumentsPassed () {
+	
+	let argumentCount = arguments.length;
+
+	// Let's do an if condition
+	if (argumentCount === 0) {
+	console.log("No argument");
+
+	} else if (argumentCount === 1){
+	console.log("Argument found");
+	} else {
+	console.log("Arguments found")
+	}
+}
+
+// now let's display these using console.log
+// using the console.log will display 'undefined' 
+// we can use only the function without console.log to print only message
+
+console.log(argumentsPassed());
+console.log(argumentsPassed(1));
+console.log(argumentsPassed(1,2,3);
