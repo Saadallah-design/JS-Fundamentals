65288ca31f69abac2d75a4595d3254e51aaf6bc1
Saadallah-design <saadalla.publishing@gmail.com>
2025-09-09 09:12:48 +0700

    Adding the one line console log 3 lines task in file 1-multi_languages.js

---
+++ 1-multi-languages.js
@@ -0,0 +1,5 @@
+let firstMsg = "C is fun";
+let secondMsg = "Python is cool";
+let thirdMsg = "JavaScript is amazing";
+                         
+console.log(firstMsg + " " + secondMsg + " " + thirdMsg);
