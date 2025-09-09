d35a410ca6758ee1b12bba640452047956f566b2
Saadallah-design <saadalla.publishing@gmail.com>
2025-09-09 09:21:16 +0700

    Renaming then adding the one line console log 3 lines task in file 1-multi_languages.js

--- 1-multi-languages.js
+++
@@ -1,5 +0,0 @@
-let firstMsg = "C is fun";
-let secondMsg = "Python is cool";
-let thirdMsg = "JavaScript is amazing";
-                         
-console.log(firstMsg + " " + secondMsg + " " + thirdMsg);
---
+++ 1-multi_languages.js
@@ -0,0 +1,8 @@
+// Here im using the backstick `` to create three lines using just one variable.
+// I can use also the \n to skip lines.
+
+let threeLinesMsg = `C is fun
+Python is cool
+JavaScript is amazing`;
+
+console.log(threeLinesMsg);
