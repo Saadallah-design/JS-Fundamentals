4b7844230bbef93820a0d9c0ef66f80290ee5c63
Saadallah-design <saadalla.publishing@gmail.com>
2025-09-09 11:01:26 +0700

    Now trying another approach with node instead of script.

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,25 +1,15 @@
 // In these lines of code I will try to implement a count arguments counter
 // first I will start by declaring a function that takes no arguments
 // then inside it I will declare a variable that equals arguments length
+// the test says that arguments should be passed to the script
 
-function argumentsPassed () {
-	
-	let argumentCount = arguments.length;
+const args = process.argv.slice(2);
+const argumentCount = args.length;
 
-	// Let's do an if condition
-	if (argumentCount === 0) {
-	console.log("No argument");
-
-	} else if (argumentCount === 1){
-	console.log("Argument found");
-	} else {
-	console.log("Arguments found")
-	}
+if(argumentCount === 0) {
+console.log("No argument");
+} else if(argumentCount === 1){
+console.log("Argument found");
+} else {
+console.log("Arguments found");
 }
-
-// now let's display these using console.log
-// using the console.log will display 'undefined' 
-// we can use only the function without console.log to print only message
-// the task i think only specifies using console inside the function so I deleted it from below
-
-argumentsPassed(1);
