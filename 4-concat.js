8752ffdd20fd249d0d11cbace8de687c5358b7ca
Saadallah-design <saadalla.publishing@gmail.com>
2025-09-14 01:42:19 +0700

    second trial 4-concat.js.

--- 4-concat.js
+++ 4-concat.js
@@ -1,8 +1,11 @@
 const args = process.argv.slice(2);
 const argument = args;
 
-if(argument) {
-    console.log(`${argument} is ${argument}`)
-} else if (argument === undefined){
+
+if(!argument[0] === undefined){
+    console.log("undefined is undefined")
+} else if(argument[1] === undefined) {
     console.log(`${argument} is Undefined`)
+} else {
+    console.log(argument[0] + " is " + argument[1])
 }
