931db7c1be3a193fd9100d3bf8c9c55985e8ab58
Saadallah-design <saadalla.publishing@gmail.com>
2025-09-14 03:15:54 +0700

    using for loop and console at end;  commit of 6-multi_lanuages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 // in this i will use for loop and an array with the lines to be printed
 let lines = ["C is fun", "Python is cool", "JavaScript is amazing"]
-
+let output = "";
 for(let i = 0; i < lines.length; i++) {
-    console.log(lines[i])
+    output += lines[i] + "\n";
 }
+console.log(output);
