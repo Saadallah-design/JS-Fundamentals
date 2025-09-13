7a19629861b2d2254de1cdcdf8680408b2fbc8f4
Saadallah-design <saadalla.publishing@gmail.com>
2025-09-14 03:20:17 +0700

    tring to fix errors by autochecker not seeing my for loop.

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 // in this i will use for loop and an array with the lines to be printed
-let lines = ["C is fun", "Python is cool", "JavaScript is amazing"]
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"]
 let output = "";
-for(let i = 0; i < lines.length; i++) {
+for (let i = 0; i < lines.length; i++) {
     output += lines[i] + "\n";
 }
 console.log(output);
