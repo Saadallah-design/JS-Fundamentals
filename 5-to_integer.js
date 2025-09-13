a4dc756aa73518921c5efee98c3c4d596c1c8704
Saadallah-design <saadalla.publishing@gmail.com>
2025-09-14 02:37:58 +0700

    initial commit of 5-concat.js

---
+++ 5-to_integer.js
@@ -0,0 +1,22 @@
+
+// Write a script that prints My number: <first argument converted in integer> 
+// if the first argument can be converted to an integer:
+// If the argument can’t be converted to an integer, print “Not a number”
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+// You are not allowed to use try/catch
+
+const args = process.argv.slice(2);
+let userInput = args[0];
+
+// using a variable called number to store user input if it is a number 
+const number = Number(userInput);
+
+if(isNaN(number) || userInput === undefined)  {
+    console.log("Not a number")
+}   else if (!Number.isInteger(number) || userInput == number) {
+    let intNumber = Math.trunc(number);
+    userInput = intNumber;
+    console.log("My number is " + userInput)
+}   
+
