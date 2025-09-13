28f50d930fb44b829e6b8e61c9495982052b5d2a
Saadallah-design <saadalla.publishing@gmail.com>
2025-09-14 03:33:14 +0700

    the last line needs to be removed.

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -2,9 +2,6 @@
 const lines = ["C is fun", "Python is cool", "JavaScript is amazing"]
 let output = "";
 for (let i = 0; i < lines.length; i++) {
-    output += lines[i];
-    if (i < lines.length - 1) {
-        output += "\n";
-    }
+    output += lines[i] + "\n";
 }
 console.log(output);
