61ea464ae8412322daf155933fe33db48c6a6149
Saadallah-design <saadalla.publishing@gmail.com>
2025-09-14 03:08:55 +0700

    initial commit of 6-multi_lanuages_loop.js

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+// in this i will use for loop and an array with the lines to be printed
+let lines = ["C is fun", "Python is cool", "JavaScript is amazing"]
+
+for(let i = 0; i < lines.length; i++) {
+    console.log(lines[i])
+}
