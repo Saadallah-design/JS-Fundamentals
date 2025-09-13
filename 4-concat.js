c14b793efdecf3505e0db00e504ce84138eb5e7c
Saadallah-design <saadalla.publishing@gmail.com>
2025-09-14 01:53:32 +0700

    4th trial 4-concat.js. fix line 5 removing

--- 4-concat.js
+++ 4-concat.js
@@ -2,7 +2,7 @@ const args = process.argv.slice(2);
 const argument = args;
 
 
-if(!argument[0] === undefined){
+if(argument[0] === undefined){
     console.log("undefined is undefined")
 } else if(argument[1] === undefined) {
     console.log(`${argument} is undefined`)
