d37a1b3e5c6f6d7f1afda9b0ac56272d1d319606
Saadallah-design <saadalla.publishing@gmail.com>
2025-09-21 22:07:11 +0700

    updating the 6-multi_languages_loop.js to match autochecker 46 characters by removing final newline with substring method.

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -3,5 +3,10 @@ const lines = ["C is fun", "Python is cool", "JavaScript is amazing"]
 let output = "";
 for (let i = 0; i < lines.length; i++) {
     output += lines[i] + "\n";
+
+    // We must remove this trailing newline to match the expected output.
+    // We can use the substring() method for this.
+    output = output.substring(0, output.length - 1);
 }
+// Print the final, single string
 console.log(output);
